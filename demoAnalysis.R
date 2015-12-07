1a0a28fa3a115e315701065243876456870c4d46
Soon Jae Kwon <kwonsjay@gmail.com>
2015-12-07 11:21:03 -0500

    add methods for drawing world map of reports

--- demoAnalysis.R
+++ demoAnalysis.R
@@ -3,6 +3,7 @@
 
 #Import libraries
 library(dplyr)
+library(rworldmap)
 
 #Import general info
 load("./EPID600_Kwon_RData/demo.general.info")
@@ -26,4 +27,20 @@ for (i in 1:nrow(files)) {
   gc()
 }
 
+#Save merged dataframe for later use
 save(list = "demo", file = "./EPID600_Kwon_RData/all.demographic.RData")
+
+#Draw world map of adverse event reports
+regions <- demo %>%
+filter(!is.na(reporter_country)) %>%
+filter(nchar(as.character(reporter_country)) == 2) %>%
+group_by(reporter_country) %>%
+summarise(count = n())
+
+sPDF <- joinCountryData2Map(regions, joinCode = "ISO2", nameJoinColumn = "reporter_country")
+par(mai = c(0,0,0.2,0), xaxs = "i", yaxs = "i")
+mapCountryData(sPDF, nameColumnToPlot = "count", mapTitle = "Adverse Event Reports by Country")
+#mapParams <- mapCountryData(sPDF, nameColumnToPlot = "count", addLegend = F)
+#do.call(addMapLegend, c(mapParams, legendWidth = 0.5, legendMar = 2))
+dev.copy(pdf, "worldmap.pdf")
+dev.off()
